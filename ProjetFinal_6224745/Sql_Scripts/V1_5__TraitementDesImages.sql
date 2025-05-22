ba0a07a377fb1ab96366de34ff3db86b35d5c724
Gerard <6224745@cegepmontpetit.ca>
2025-05-22 15:39:33 -0400

    debut pour les images

---
+++ ProjetFinal_6224745/Sql_Scripts/V1_5__TraitementDesImages.sql
@@ -0,0 +1,40 @@
+CREATE TABLE Appareil.Affiche(
+	AfficheID int IDENTITY(1,1) NOT NULL,
+	Identifiant uniqueidentifier NOT NULL ROWGUIDCOL,
+	AgreID int NOT NULL,
+	CONSTRAINT PK_Appareil_AfficheID PRIMARY KEY (AfficheID)
+)
+GO
+
+ALTER TABLE Appareil.Affiche ADD CONSTRAINT UC_Appareil_Identifiant UNIQUE(Identifiant)
+GO
+
+ALTER TABLE Appareil.Affiche ADD CONSTRAINT DF_Appareil_Identifiant DEFAULT newid() FOR Identifiant
+GO
+
+ALTER TABLE Appareil.Affiche ADD AfficheContent varbinary(max) FILESTREAM NULL
+GO
+
+INSERT INTO Appareil.Affiche(AgreID, AfficheContent)
+SELECT 1, BulkColumn FROM OPENROWSET(
+	BULK 'C:\Users\GERI\Desktop\ProjetFinal_6224745\Images\Sol.png', SINGLE_BLOB) AS myfile
+
+INSERT INTO Appareil.Affiche(AgreID, AfficheContent)
+SELECT 2, BulkColumn FROM OPENROWSET(
+	BULK 'C:\Users\GERI\Desktop\ProjetFinal_6224745\Images\Arcon.png', SINGLE_BLOB) AS myfile
+
+INSERT INTO Appareil.Affiche(AgreID, AfficheContent)
+SELECT 3, BulkColumn FROM OPENROWSET(
+	BULK 'C:\Users\GERI\Desktop\ProjetFinal_6224745\Images\Anneaux.png', SINGLE_BLOB) AS myfile
+
+INSERT INTO Appareil.Affiche(AgreID, AfficheContent)
+SELECT 4, BulkColumn FROM OPENROWSET(
+	BULK 'C:\Users\GERI\Desktop\ProjetFinal_6224745\Images\Saut.png', SINGLE_BLOB) AS myfile
+
+INSERT INTO Appareil.Affiche(AgreID, AfficheContent)
+SELECT 5, BulkColumn FROM OPENROWSET(
+	BULK 'C:\Users\GERI\Desktop\ProjetFinal_6224745\Images\Parra.png', SINGLE_BLOB) AS myfile
+
+INSERT INTO Appareil.Affiche(AgreID, AfficheContent)
+SELECT 6, BulkColumn FROM OPENROWSET(
+	BULK 'C:\Users\GERI\Desktop\ProjetFinal_6224745\Images\Fixe.png', SINGLE_BLOB) AS myfile
